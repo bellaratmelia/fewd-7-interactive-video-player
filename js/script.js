34b9504628a3ac6d97e4424c1fb546b06ab4a751
Bella Ratmelia <bella.ratmelia@gmail.com>
2016-06-26 15:19:04 +0800

    major update
    - changed font
    - prep transcript
    - add the “skip to” function when transcript is clicked
    - add the function to highlight current transcript being spoken on the
    video

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ if (supports_video) {
 	var video_controls = document.getElementById('video-controls');
 	var left_video_controls = document.getElementById('left-controls');
 	var right_video_controls = document.getElementById('right-controls');
+	var transcript_area = document.getElementById('transcript');
 
 	// hide default controls
 	video.controls = false;
@@ -22,17 +23,21 @@ if (supports_video) {
 	var fullscreen_button = document.getElementById('full-screen');
 	var current_time = document.getElementById('current-time');
 	var duration = document.getElementById('duration');
+	var caption_button = document.getElementById('closed-caption');
 
 	// transcript text area
 	var transcript_text = document.getElementById('transcript');
 
 	// some utilities vars
-	var play_state = '<img src="icons/play-icon.png" />';
-	var pause_state = '<img src="icons/pause-icon.png" />';
-	var mute_state = '<img src="icons/volume-off-icon.png" />';
-	var unmute_state = '<img src="icons/volume-on-icon.png" />';
+	var play_state = '<img src="icons/play-icon.png" alt="play video" />';
+	var pause_state = '<img src="icons/pause-icon.png" alt="pause video" />';
+	var mute_state = '<img src="icons/volume-off-icon.png" alt="mute video" />';
+	var unmute_state = '<img src="icons/volume-on-icon.png" alt="unmute video" />';
 
-	// transcript text with start and finish
+	// placeholder for transcript object;
+	var transcript_obj;
+
+	// Array of object for transcript text with start and finish
 	var transcript_array = [
 		{"start": "0.00",
 			"fin": "7.535",
@@ -57,9 +62,47 @@ if (supports_video) {
 			"text": "A modem is what connects the internet to your network at home."},
 		{"start": "57.781",
 			"fin": "59.000",
-			"text": "A few common residential modems are DSL or..."}   
+			"text": "A few common residential modems are DSL or--"}   
 	];
 
+	// Utility function to format time nicely
+	function format_time(seconds) {
+	    minutes = Math.floor(seconds / 60);
+	    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+	    seconds = Math.floor(seconds % 60);
+	    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+	    return minutes + ":" + seconds;
+	}
+
+	// Skip video to the time specified in span ID
+	function skip_to_text(e) {
+		video.currentTime = e.target.id;
+		video.play();
+	}
+
+	// Place text inside the #transcript with appropriate span and id
+	function prep_transcript() {
+		var temp;
+		for (var i = 0; i < transcript_array.length; i++) {
+			// create a span element
+			temp = document.createElement('span');
+
+			// put the text to the span element
+			temp.innerHTML = transcript_array[i].text + ' ';
+
+			// set the id to the time of start of the cue
+			temp.setAttribute('id', transcript_array[i].start);
+
+			// append the element to transcript area
+			transcript_area.appendChild(temp);
+
+			// attach event listener that will fire skip_to_text when span is clicked
+			temp.addEventListener('click', skip_to_text);
+		}
+	}
+
+	// Function calls
+	prep_transcript();
 
 	// set the duration of the video
 	video.addEventListener('loadedmetadata', function() {
@@ -88,7 +131,7 @@ if (supports_video) {
 		if (video.muted == true) {
 			// unmute video
 			video.muted = false;
-			volume_bar.value = 50;
+			volume_bar.value = 100;
 
 			// change button icon to 'unmuted'
 			mute_button.innerHTML = unmute_state;
@@ -122,7 +165,8 @@ if (supports_video) {
 		video.currentTime = time;
 	});
 	
-	// Update video when progress bar changed
+	// Update video when progress bar changed, and-
+	// During video play, check for time and place #highlight on the span to change the color of the text
 	video.addEventListener("timeupdate", function() {
 		// Calculate the slider value
 		var value = (100 / video.duration) * video.currentTime;
@@ -132,7 +176,28 @@ if (supports_video) {
 
 		// update current-time value;
 		current_time.innerHTML = format_time(video.currentTime);
+
+		// highlight the appropriate transcript span
+		for (var i = 0; i < transcript_array.length; i++) {
+
+			// find the span element with the correct id
+			if (video.currentTime >= transcript_array[i].start && video.currentTime <= transcript_array[i].fin) {
+				
+				// remove .highlight from all span elements first, and then
+				var the_spans = transcript_area.getElementsByTagName('span');
+				for (var s = 0; s < the_spans.length; s++) {
+					the_spans[s].classList.remove('highlight');
+	            }
+
+				// append .highlight to the correct span
+				document.getElementById(transcript_array[i].start).classList.add('highlight');
+
+			}
+		}
 	});
+
+	var the_spans = transcript_area.getElementsByTagName('span');
+	console.log(the_spans);
 
 	// Update video playback when progress bar is clicked anywhere
 	progress_bar.addEventListener('click', function(e) {
@@ -152,13 +217,24 @@ if (supports_video) {
 			mute_button.innerHTML = unmute_state;
 		}
 	});
-	
-}
 
-function format_time(seconds) {
-    minutes = Math.floor(seconds / 60);
-    minutes = (minutes >= 10) ? minutes : "0" + minutes;
-    seconds = Math.floor(seconds % 60);
-    seconds = (seconds >= 10) ? seconds : "0" + seconds;
-    return minutes + ":" + seconds;
+	// Caption button click event
+	caption_button.addEventListener('click', function() {
+		if (video.textTracks[0].mode == "showing") {
+            	// Turn off captions
+            	video.textTracks[0].mode = "hidden";
+
+            	// update CC button text 
+                caption_button.innerHTML = "<strike>CC</strike>";
+            } else {
+            	// Turn captions on
+            	video.textTracks[0].mode = "showing";
+
+            	// update CC button text 
+                caption_button.innerHTML = "CC";
+            }    
+        });
+
+	
+		
 }
